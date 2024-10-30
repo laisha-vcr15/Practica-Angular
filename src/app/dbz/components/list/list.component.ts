32c8973199c635a8237b50970dbcd942c59b093a
Laisha Vanessa Contreras Rodriguez <laisha.contreras@imss.gob.mx>
2024-10-30 16:54:45 -0600

    Cambios de personaje

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  public characterList: Character[] = [{
+    name: 'Aang',
+    power: 10
+  }]
 }
--- src/app/dbz/pages/main-pages.component.ts
+++ src/app/dbz/pages/main-pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -7,4 +8,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class MainPageComponent {
     
+    public characters: Character[] = [{
+        name: 'Krilin',
+        power: 400
+    }, {
+        name: 'Azula',
+        power: 24
+    }];
 }
